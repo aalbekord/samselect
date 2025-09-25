d1e8eb589bedfc26c24c78b9e47746bd7e7a003b
amirmatics <4miralbekord@gmail.com>
2025-09-24 18:22:48 -0700

    created buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [samosas, setSamosas] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  
+  const handleClick = () => {
+    setSamosas(samosas + multiplier)
+  }
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+    }
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>{`Count: ${samosas}`}</h2>
+        <img onClick={handleClick} className="samosa" src="https://static.vecteezy.com/system/resources/previews/050/018/866/non_2x/fresh-tasty-samosa-cutout-isolated-on-transparent-background-ideal-for-product-banners-social-media-posts-and-creative-cover-designs-free-png.png" alt="samosa" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
